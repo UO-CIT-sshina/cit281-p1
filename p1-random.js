78c2f67ded5d9dd46439b5eb63777c73c15dbd72
sshina <105396051+sshina@users.noreply.github.com>
2022-06-02 17:54:47 -0700

    Add files via upload

---
+++ p1-random.js
@@ -0,0 +1,16 @@
+/*
+    CIT 281 Project 1
+    Name: Shinault
+*/
+// Courtesy of the assignment page
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function randomLower(){
+    let result = "";
+    let limit = getRandomInteger(5, 26);
+    console.log(limit);
+    for (let i = 0; i < limit; i++) result += "qwertyuiopasdfghjklzxcvbnm"[Math.floor(Math.random()*26)];
+    return result;
+}
+console.log(randomLower());
